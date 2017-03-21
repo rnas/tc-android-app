d6969abfb86466dae911ede8716f85abbc290a5d
rnas <rnaswaty@gmail.com>
2017-03-20 23:46:16 -0300

    [fix] bug form submit

--- Brejas/app/src/main/java/brejas/com/br/brejas/NewItem.java
+++ Brejas/app/src/main/java/brejas/com/br/brejas/NewItem.java
@@ -63,7 +63,59 @@ public class NewItem extends AppCompatActivity {
             btSubmit.setText(R.string.new_add);
         }
 
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                if ( validateDataEntry() ) {
+
+                    BeersDatabase db = new BeersDatabase(getBaseContext());
+
+                    if (editing == null) {
+
+
+                        Beer newItem = new Beer(txName.getText().toString(), txBrand.getText().toString(), Integer.parseInt(txContent.getText().toString()), Integer.parseInt(txUnits.getText().toString()));
+
+                        db.addItem(newItem);
+                        db.close();
+
+                        Intent intent = new Intent();
+                        intent.putExtra("new_item", (Parcelable) newItem);
+                        setResult(RESULT_OK, intent);
+                        finish();
+
+                    } else {
+
+                        editing.setName(txName.getText().toString());
+                        editing.setBrand(txBrand.getText().toString());
+                        editing.setUnits(Integer.parseInt(txUnits.getText().toString()));
+                        editing.setContent(Integer.parseInt(txContent.getText().toString()));
+
+                        db.updateItem(editing);
+                        db.close();
+
+                        Intent intent = new Intent();
+                        intent.putExtra("new_item", (Parcelable) editing);
+                        setResult(RESULT_OK, intent);
+                        finish();
+
+                    }
+
+                } else {
+
+                    showErrorMessage(formError.toString());
+
+                }
+
+            }
+        });
+
+
+
+    }
+
+    void showErrorMessage (String errorMSG) {
+        Toast.makeText(this, errorMSG, Toast.LENGTH_LONG).show();
     }
 
 //    void initItemsSpinner() {
